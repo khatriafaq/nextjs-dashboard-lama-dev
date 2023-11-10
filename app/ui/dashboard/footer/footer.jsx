0018c5c8e2b6b941bd665e9196cd2a240b95c412
Afaq Ahmed <afaq.ahmed@inaequostudios.com>
2023-11-10 18:35:51 +0500

    ui/dashboard working

---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./footer.module.css"
+
+
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.logo}>Lama Dev</div>
+        <div className={styles.text}>All Rights Reserved</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -0,0 +1,18 @@
+"use client"
+import styles from "./menuLink.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const MenuLink = ({ item }) => {
+    const pathname = usePathname()
+    
+  return (
+    <Link href={item.path} className={`${styles.container} ${pathname === item.path && styles.active}`} >
+      {item.icon}
+      {item.title}
+    </Link>
+  );
+};
+
+export default MenuLink;
